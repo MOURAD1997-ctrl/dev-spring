6c1fe95fb064b213f84bbed03e3f1d5b75471789
MOURAD1997-ctrl <elabbadimourad9@gmail.com>
2020-06-24 15:32:36 +0100

    First Commit

---
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,29 @@
+package com.luv2code.springdemo.mvc.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+
+	
+	private String coursePrefixe;
+	
+	@Override
+	public void initialize(CourseCode theCourseCode) {
+		coursePrefixe = theCourseCode.value();
+	}
+	
+	@Override
+	public boolean isValid(String theCode, ConstraintValidatorContext theConstraintValidatorContext ) {
+         boolean result;
+         if (theCode != null) {
+        	 result = theCode.startsWith(coursePrefixe);
+         }
+         else {
+        	 result = true;
+         }
+		
+		return result;
+	}
+
+}
